5cce5d72246066108d7f6d6f70f3e53e352b60a1
Petyo Ivanov <underlog@gmail.com>
2023-12-27 15:24:22 +0200

    Testing mdxeditor with CSP enabled

---
+++ app/EditorComponent.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { diffSourcePlugin, headingsPlugin, listsPlugin, quotePlugin, thematicBreakPlugin, markdownShortcutPlugin, MDXEditor, MDXEditorMethods } from "@mdxeditor/editor"
+
+import {FC} from 'react'
+
+interface EditorProps {
+  markdown: string
+  editorRef?: React.MutableRefObject<MDXEditorMethods | null>
+}
+
+/**
+ * Extend this Component further with the necessary plugins or props you need.
+ * proxying the ref is necessary. Next.js dynamically imported components don't support refs. 
+*/
+const Editor: FC<EditorProps> = ({ markdown, editorRef }) => {
+  return <MDXEditor ref={editorRef} markdown={markdown} plugins={[  headingsPlugin(),
+    listsPlugin(),
+    quotePlugin(),
+    thematicBreakPlugin(),
+    markdownShortcutPlugin(),
+    diffSourcePlugin(),
+    // the plugins below will cause problems with csp
+    // codeBlockPlugin(),
+    // sandpackPlugin()
+  ]} />
+}
+
+export default Editor
---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import dynamic from 'next/dynamic'
+import { Suspense } from 'react'
+
+const EditorComp = dynamic(() => import('./EditorComponent'), { ssr: false })
+
+const markdown = `
+# Hello world!
+Check the EditorComponent.tsx file for the code .
+`
+export default function Home() {
+  return (
+    <div>Editor:
+      <Suspense fallback={null}>
+        <EditorComp markdown={markdown} />
+      </Suspense>
+    </div>
+  )
+}
